0633b9b6ba62196d34519368dd0b1908c2c59f0d
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-06-27 18:55:54 +0700

    update

---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const apps = express();
+const bodyParser = require('body-parser')
+const port = process.env.PORT;
+const path = require('path');
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '123',
+    port: 5432,
+})
+
+apps.use(bodyParser.json())
+apps.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+apps.use(express.static('public'));
+
+apps.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/index.html'));
+})
+
+
+apps.get('/detail/:id', (req, res) => {
+    res.sendFile(path.resolve('./views/detail.html'));
+    // const idTx = req.params.id;
+    // console.log(idTx)
+})
+
+apps.get('/portofolio', (req, res) => {
+    pool.query('SELECT * FROM tbl_portofolio ORDER BY id DESC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
+
+apps.get('/portofolio/detail/:id', (req, res) => {
+    const idTx = req.params.id;
+    pool.query('SELECT * FROM tbl_portofolio WHERE id = $1 ' , [idTx] , (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
+apps.listen(port);
