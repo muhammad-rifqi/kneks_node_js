2260e5d5540169c49a851b7e3cbc34ec358b73cf
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-08-12 14:12:25 +0700

    update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres',
+    database: 'kneks',
     password: 'Barlok15@',
     port: 5432,
 })
