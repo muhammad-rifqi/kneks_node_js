28f4a4c61a386dcfc0a01e74730ac6972a877208
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-08-13 19:32:26 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const apps = express();
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const port = process.env.PORT;
 const path = require('path');
