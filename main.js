3aba6c66d44563a05a6586128ec184154f8c1515
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-08-14 16:59:47 +0700

    update

--- main.js
+++ main.js
@@ -5,16 +5,25 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const port = process.env.PORT;
 const path = require('path');
+const mysql = require('mysql2');
+var md5 = require('md5');
+// const Pool = require('pg').Pool
 
-const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'kneks',
+//     password: 'Barlok15@',
+//     port: 5432,
+// })
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'kneks',
-    password: 'Barlok15@',
-    port: 5432,
-})
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kneks"
+});
+
 
 apps.use(bodyParser.json())
 apps.use(
@@ -26,9 +35,12 @@ apps.use(cors());
 apps.use(express.static('public'));
 
 apps.get('/', (req, res) => {
-    res.sendFile(path.resolve('./views/index.html'));
+    res.sendFile(path.resolve('./views/login.html'));
 })
 
+apps.get('/dashboard', (req, res) => {
+    res.sendFile(path.resolve('./views/dashboard.html'));
+})
 
 apps.get('/detail/:id', (req, res) => {
     res.sendFile(path.resolve('./views/detail.html'));
@@ -37,40 +49,67 @@ apps.get('/detail/:id', (req, res) => {
 })
 
 apps.get('/posts', (req, res) => {
-    pool.query('SELECT * FROM posts ORDER BY id DESC', (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM news ORDER BY id DESC", function (err, result) {
+            if (err) throw err;
+            res.status(200).json(result)
+        });
+    });
 })
-
 
 apps.get('/posts/type/:name', (req, res) => {
     const names = req.params.name;
-    pool.query('SELECT * FROM posts WHERE type = $1 ' , [names] , (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query('SELECT * FROM news_' + names + '', function (err, result) {
+            if (err) throw err;
+            res.status(200).json(result)
+        });
+    });
 })
 
 apps.post('/do_login', (req, res) => {
     const email = req?.body?.email;
-    const password = req?.body?.password;
-    pool.query('SELECT * FROM users where email = $1 AND password = $2',  [email , password] , (error, results) => {
-        if (error) {
-            throw error
-        }
-    if(results?.rows?.length > 0){
-        res.status(200).json(results.rows)
-    }else{
-        res.status(200).json({"success": false})
-    }
-        
+    const password = md5(req?.body?.password);
+
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query('SELECT * FROM users where email = ? AND password = ? ', [email, password], (error, results) => {
+            if (error) {
+                throw error
+            }
+            if (results?.length > 0) {
+                res.status(200).json(results)
+            } else {
+                res.status(200).json({ "success": false })
+            }
+        });
     })
 })
 
+apps.post('/act_login', (req, res) => {
+    const email = req?.body?.email;
+    const password = md5(req?.body?.password);
+    console.log(email, password)
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query('SELECT * FROM users where email = ? AND password = ? ', [email, password], (error, results) => {
+            if (error) {
+                throw error
+            }
+            if (results?.length > 0) {
+                console.log(results)
+                res.redirect("/dashboard");
+            } else {
+                res.redirect("/");
+            }
+        });
+    })
+})
 
-apps.listen(5000);
+apps.get("/logout", (req, res) => {
+    res.redirect("/");
+  });
+
+apps.listen(port);
